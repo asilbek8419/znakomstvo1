b56882d75839fcc794171f12c94e8d500bd9c506
Asilbek Normatov <asilbek84@bk.ru>
2023-09-28 19:16:32 +0500

    2zadanie

---
+++ 2zadanie/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+Console.Write("Введите 1-oe число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите 2-oe число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите 3-ee число: ");
+int c = Convert.ToInt32(Console.ReadLine());
+int max = c;
+
+if (a > max)
+    Console.Write(a);
+    
+    else if (b > max)
+    Console.Write(b);
+    
+else
+    Console.Write(c);
